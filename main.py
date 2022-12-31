68e70708f2b79c0b46a84816266cba77c47765dd
A1venG0 <artemlev11@ukr.net>
2022-12-31 22:02:11 +0200

    Added: Password hash implementation

---
+++ main.py
@@ -0,0 +1,35 @@
+import redis
+import hashlib
+
+r = redis.Redis(host="localhost", port=6379, db=0)
+
+def schedule_row_cache(conn, row_id, delay):
+	conn.zadd('delay:', row_id, delay)
+	conn.zadd('schedule:', row_id, time.time())
+
+def cache_rows(conn):
+	while not QUIT:
+		next = conn.zrange('schedule:', 0, 0, withscores=True)
+		now = time.time()
+		if not next or next[0][1] > now:
+			time.sleep(.05)
+			continue
+
+		row_id = next[0][0]
+		delay = conn.zscore('delay:', row_id)
+		if delay <= 0:
+			conn.zrem('delay:', row_id)
+			conn.zrem('schedule:', row_id)
+			conn.delete('inv:' + row_id)
+			continue
+
+		row = Inventory.get(row_id)
+		conn.zadd('schedule:', row_id, now + delay)
+		conn.set('inv:' + row_id, json.dumps(row.to_dict()))
+
+def hash_password(password):
+	hashed_password = hashlib.sha256(password.encode('utf-8').hexdigest())
+	return hashed_password
+
+if __name__ == "__main__":
+	#r = redis.Redis(host="localhost", port=6379, db=0)
