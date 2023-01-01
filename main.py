d106a2ac42c8265c38e4d624dc925000707cab25
A1venG0 <artemlev11@ukr.net>
2023-01-01 14:04:21 +0200

    Fixed: function defenitions

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import redis
 import hashlib
+import time
 
 class MyRedis:
 	def __init__(self, host_name="localhost", port=6379, db=0):
 		self.r = redis.Redis(host_name, port, db)
 
-	def update_token(login, password): # update user login info in the cache
+	def update_token(self, login, password): # update user login info in the cache
 		timestamp = time.time()
-		r.hset('login:', login, password)
-		r.zadd('recent:', login, timestamp)
+		self.r.hset('login:', login, password)
 
 
-	def check_token(login):
-		return r.hget('login:', login) # fetch and return a given user, if available
+	def check_token(self, login):
+		return self.r.hget('login:', login) # fetch and return a given user, if available
 
-	def hash_password(password): # hash user password using SHA-256 hashing algorithm
+	def hash_password(self, password): # hash user password using SHA-256 hashing algorithm
 		hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 		return hashed_password
 
-if __name__=="__main__":
-	myRedis = MyRedis()
+
