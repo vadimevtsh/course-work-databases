e1cd465e7ae303565ff5e7bef577375b9ed8edaf
A1venG0 <artemlev11@ukr.net>
2023-01-01 13:42:44 +0200

    Added: updating login

--- main.py
+++ main.py
@@ -1,40 +1,19 @@
 import redis
 import hashlib
 
-r = redis.Redis(host="localhost", port=6379, db=0)
+class MyRedis:
+	def __init__(self, host_name="localhost", port=6379, db=0)
+	self.r = redis.Redis(host_name, port, db)
 
-"""
-def schedule_row_cache(conn, row_id, delay):
-	conn.zadd('delay:', row_id, delay)
-	conn.zadd('schedule:', row_id, time.time())
+	def update_token(login, password): # update user login info in the cache
+		timestamp = time.time()
+		r.hset('login:', login, password)
+		r.zadd('recent:', login, timestamp)
 
-def cache_rows(conn):
-	while not QUIT:
-		next = conn.zrange('schedule:', 0, 0, withscores=True)
-		now = time.time()
-		if not next or next[0][1] > now:
-			time.sleep(.05)
-			continue
 
-		row_id = next[0][0]
-		delay = conn.zscore('delay:', row_id)
-		if delay <= 0:
-			conn.zrem('delay:', row_id)
-			conn.zrem('schedule:', row_id)
-			conn.delete('inv:' + row_id)
-			continue
+	def check_token(login):
+		return r.hget('login:', login) # fetch and return a given user, if available
 
-		row = Inventory.get(row_id)
-		conn.zadd('schedule:', row_id, now + delay)
-		conn.set('inv:' + row_id, json.dumps(row.to_dict()))
-"""
-def check_token(conn, login):
-	return conn.hget('login:', login) # fetch and return a given user, if available
-
-def hash_password(password):
-	hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
-	return hashed_password
-
-if __name__ == "__main__":
-	print(hash_password("1234"))
-	#r = redis.Redis(host="localhost", port=6379, db=0)
+	def hash_password(password): # hash user password using SHA-256 hashing algorithm
+		hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
+		return hashed_password
