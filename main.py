c3714852b19d649a6986aa52747661eca8054f98
A1venG0 <artemlev11@ukr.net>
2023-01-01 13:20:37 +0200

    Added: checking login inside hash table

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import hashlib
 
 r = redis.Redis(host="localhost", port=6379, db=0)
 
+"""
 def schedule_row_cache(conn, row_id, delay):
 	conn.zadd('delay:', row_id, delay)
 	conn.zadd('schedule:', row_id, time.time())
@@ -26,10 +27,14 @@ def cache_rows(conn):
 		row = Inventory.get(row_id)
 		conn.zadd('schedule:', row_id, now + delay)
 		conn.set('inv:' + row_id, json.dumps(row.to_dict()))
+"""
+def check_token(conn, login):
+	return conn.hget('login:', login) # fetch and return a given user, if available
 
 def hash_password(password):
-	hashed_password = hashlib.sha256(password.encode('utf-8').hexdigest())
+	hashed_password = hashlib.sha256(password.encode('utf-8')).hexdigest()
 	return hashed_password
 
 if __name__ == "__main__":
+	print(hash_password("1234"))
 	#r = redis.Redis(host="localhost", port=6379, db=0)
