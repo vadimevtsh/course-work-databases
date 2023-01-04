8548dca2147af43879ecb72c9ef4c41d91ecb9d4
vadimevtsh <vadim.evtushenko1@gmail.com>
2023-01-05 00:54:25 +0200

    Added: HTMl/CSS - Reworked web

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
+from flask_services import app
 
-app = Flask(__name__)
-
-@app.route("/")
-def home_page():
-    return render_template('index.html')
-
-
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
