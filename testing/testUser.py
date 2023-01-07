d16a60feb2812c5719bbea77c51d57752a6e5642
A1venG0 <artemlev11@ukr.net>
2023-01-07 20:55:00 +0200

    Testing: initial commit

---
+++ testing/testUser.py
@@ -0,0 +1,17 @@
+import unittest
+
+class TestUserMethods(unittest.TestCase):
+
+	def test_user_create(self):
+
+	def test_user_exists(self):
+
+	def test_user_delete(self):
+
+	def test_add_identical_user(self):
+
+	def test_delete_nonexisting_user(self):
+
+
+if __name__ == "__main__":
+	unittest.main();
