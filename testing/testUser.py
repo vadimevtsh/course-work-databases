dfdfeb1ec5e8f2aed4b386baef0427fecb26f52e
A1venG0 <artemlev11@ukr.net>
2023-01-07 21:14:10 +0200

    Added: importing models into tests

--- testing/testUser.py
+++ testing/testUser.py
@@ -1,7 +1,12 @@
 import unittest
+import sys
+
+sys.path.insert(1, 'D:\\ArtemStudy\\3c\\db\\coursework\\flask_services')
+import models.py
 
 class TestUserMethods(unittest.TestCase):
-
+	user = User();
+	
 	def test_user_create(self):
 
 	def test_user_exists(self):
